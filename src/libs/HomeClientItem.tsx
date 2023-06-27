1b193eb449e6e82afca679843636e1b08d82abb4
Sakil Biorev <sakil.saiyed@biorev.studio>
2023-06-27 18:29:44 +0530

    client-section-changes

---
+++ src/libs/HomeClientItem.tsx
@@ -0,0 +1,5 @@
+export interface HomeClientItem {
+  id: number;
+  src: string;
+  alt: string;
+}
---
+++ src/libs/data/HomeClientsData.tsx
@@ -0,0 +1,76 @@
+import type { HomeClientItem } from '../interfaces';
+
+const HomeClientsData: HomeClientItem[] = [
+  {
+    id: 1,
+    src: '/clients/avex-logo-web-1.jpg',
+    alt: 'Image 1',
+  },
+  {
+    id: 2,
+    src: '/clients/bearhomes-1.jpg',
+    alt: 'Image 2 ',
+  },
+  {
+    id: 3,
+    src: '/clients/cristo-homes-1.jpg',
+    alt: 'Image 3',
+  },
+  {
+    id: 4,
+    src: '/clients/david-weekley-homes.jpg',
+    alt: 'Image 4',
+  },
+  {
+    id: 5,
+    src: '/clients/dw.jpg',
+    alt: 'Image 5',
+  },
+  {
+    id: 6,
+    src: '/clients/kingbridge.jpg',
+    alt: 'Image 6',
+  },
+  {
+    id: 7,
+    src: '/clients/logo.jpg',
+    alt: 'Image 7',
+  },
+  {
+    id: 8,
+    src: '/clients/london-homes.jpg',
+    alt: 'Image 8',
+  },
+  {
+    id: 9,
+    src: '/clients/maracay-1.jpg',
+    alt: 'Image 9',
+  },
+  {
+    id: 10,
+    src: '/clients/megatel-homes.jpg',
+    alt: 'Image 10',
+  },
+  {
+    id: 11,
+    src: '/clients/payne-family-homes.jpg',
+    alt: 'Image 11',
+  },
+  {
+    id: 12,
+    src: '/clients/sothebysrealty-1.jpg',
+    alt: 'Image 12',
+  },
+  {
+    id: 13,
+    src: '/clients/terry-Homes.jpg',
+    alt: 'Image 13',
+  },
+  {
+    id: 14,
+    src: '/clients/toll-brothers-1.jpg',
+    alt: 'Image 14',
+  },
+];
+
+export default HomeClientsData;
