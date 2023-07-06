8f28d672fd7bb1121e0e3ac03701e04542682bf8
Sakil Biorev <sakil.saiyed@biorev.studio>
2023-07-06 18:49:01 +0530

    testimonial data

--- src/libs/data/TestimonialsSliderData.tsx
+++ src/libs/data/TestimonialsSliderData.tsx
@@ -27,11 +27,91 @@ const TestimonialsSliderData: TestimonialsSliderItem[] = [
   },
   {
     id: 4,
-    avatar: '/images/testimonial/jack.jpg',
-    name: 'Jack Campbell',
-    designation: 'Realtor® - Broker Associate - Investor',
+    avatar: '/images/testimonial/mohan.jpeg',
+    name: 'Mohan Mahal',
+    designation: 'Founder & CEO - SIDCO Homes Inc',
     description:
-      '“Worked with Biorev team to create full interior fly through animation for 4000 SQFT residential canyon property.  Very helpful with revisions and made sure we got what we needed to fully market a home not yet built.  Thanks for a job well done!”',
+      '“I got the rendering done for a Net Zero Home in the Bay Area. The team work was excellent in finishing the project in time and on budget. I strongly recommend them for such work. Totally Satisfied.”',
+  },
+  {
+    id: 5,
+    avatar: '',
+    name: 'William Jordan',
+    designation: 'Founder & CEO - William Jordan Co',
+    description:
+      '“Sagar worked with my Bay Area condo project for a planning commission meeting presentation.  It was three large buildings for over 80 condos.  He does amazing work and the city and neighbors were impressed with the work.  The images he took from CAD files to his final drafts made my development look awesome.  Plants and trees enhanced the drawings to look like real life photos.  I highly recommend Biorev LLC. ”',
+  },
+  {
+    id: 6,
+    avatar: '/images/testimonial/brandi.jpeg',
+    name: 'Brandi Stokes',
+    designation: 'REALTOR - Cameo Homes Inc.',
+    description:
+      '“I was in a pinch and needed a rendering with a  quick turn around time for a magazine.  Sagar and Megan were ON TOP OF THINGS!  The process was incredibly easy.  I provided photos the home along with a color chart that they asked me to fill out.  I had the rendering in less than a day!!  Thanks BioRev for the excellent communication and expediency in handling our request!”',
+  },
+  {
+    id: 7,
+    avatar: '/images/testimonial/adam.jpeg',
+    name: 'Adam Pretorius',
+    designation: 'Associate Broker - Lepic Kroeger Realtors',
+    description:
+      "“I've worked with Biorev for a few months now on renderings for new construction.  They are very professional and timely, along with being very easy to work with, on any edits that I have.  Their product is also exceptional.”",
+  },
+  {
+    id: 8,
+    avatar: '/images/testimonial/lacy.jpeg',
+    name: 'Lacy Laster',
+    designation: 'Marketing Coordinator - jeff benton homes',
+    description:
+      '“I was very impressed by the quality and quickness of this company. The turnaround was really fast and the renderings they did for our company looked so realistic! I would highly recommend this company!”',
+  },
+  {
+    id: 9,
+    avatar: '/images/testimonial/paul.jpg',
+    name: 'Paul Goodner',
+    designation: 'President of Goodner Brothers Construction',
+    description:
+      '“I was introduced to Biorev at this year’s International Builders Show. We had been searching for a partner to help us create 3D renderings for our clients. Biorev’s work is outstanding. Sagar is great to work with and goes the extra mile to ensure we get exactly what we need. He even helped us with a cutaway to better show our standard features. I am looking forward to working with Biorev on many more projects. A big 5-stars!”',
+  },
+  {
+    id: 10,
+    avatar: '/images/testimonial/zach.jpg',
+    name: 'Zach Osina',
+    designation: 'Design Manager of Camillo Properties',
+    description:
+      '"It was a pleasure working with Sagar and his team at Biorev.  When we started working together on renderings, his team was at a quality level however over time the team’s rendering output improved even more so.  They were open to suggestions and flexible to meet our expectations.  Biorev’s projects were completed timely and there was never a communication issue."',
+  },
+  {
+    id: 11,
+    avatar: '',
+    name: 'Eleazar Saldana',
+    designation: 'Architectural Department Manager of Megatel Homes',
+    description:
+      '“Biorev was able to manage a large project for us in a timely manner and went beyond our quality expectations, they quickly understood our needs  and provided us with a great project management that made our project to go smooth, I will definitely recommend them and definitely continue to work with them on future projects.”',
+  },
+  {
+    id: 12,
+    avatar: '/images/testimonial/jason.jpg',
+    name: 'Jason Baxter',
+    designation: 'Vice President of Development at Fort Capital, L.P',
+    description:
+      '“I had the opportunity to work with Biorev on a very large project to produce 3D renderings and a Virtual tour for one of our largest clients. The Team at Biorev is amazing and the quality of the work was outstanding! I would recommend Biorev to anyone who is in need of a Digital Marketing solution! ”',
+  },
+  {
+    id: 13,
+    avatar: '/images/testimonial/homeInsiders.png',
+    name: 'Sarah Renwick',
+    designation: 'HomeInsiders . Exp Realty',
+    description:
+      '"Biorev took my company\'s vision and made it a reality! We, along with their amazing team built a custom real estate website that stands out and offers features unlike most. The Biorev team is top-notch; professional, knowledgeable, pleasant and best part well priced. And timely! We have future development and marketing plans and without a doubt, are partnered with the best development company for our ambition and growth."',
+  },
+  {
+    id: 14,
+    avatar: '/images/testimonial/ucp.png',
+    name: 'Leslie Frazier',
+    designation: 'President of urban chic properties',
+    description:
+      '“Biorev is responsive, timely and have done a great job for us! Wonderful customer service and final product!”',
   },
 ];
 
